a9eabf63becfad9072d3d11fdd7690eb286d5c7b
nk <devopsnike@gmail.com>
2022-08-05 13:41:38 +0530

    update

--- arithmatic.sh
+++ arithmatic.sh
@@ -8,14 +8,33 @@ add=$((a + b))
 #  not add=$(a+b)
 echo Addition of a and b is $add
 
+val=`expr $a + $b`         # value of a+b is stored in val variable
+echo "Addition of a + b : $val"
+
+
 sub=$((a - b))
 echo Subtarction of a and b is $sub
+
+val=`expr $a - $b`
+echo "Subtraction of a - b : $val"
+
 
 mul=$((a*b))
 echo 'Multiplication of a and b is '$mul
 
+val=`expr $a \* $b`
+echo "Mul of a * b : $val"
+
+
 div=$((a/b))
 echo "Division of a and b is "$div
 
+val=`expr $a / $b`
+echo "Div of a / b : $val"
+
+
 mod=$((a%b))
-Modulus of a and b is $mod
+echo Modulus of a and b is $mod
+
+val=`expr $a % $b`         # '%' gives remainder value
+echo "a % b : $val"
--- variables.sh
+++ variables.sh
@@ -1,8 +1,20 @@
 *!/bin/bash
 
-shell="bash"      # do not leave space before & after '=' while defining variables
+shell="bash"                        # do not leave space before & after '=' while defining variables
 echo "I am using $shell"
 echo "I am "${shell}ing
 
 NAME="devops"
 echo My name is ${NAME}nik
+echo What is your name?
+#read NAME                          # 'read' used to get input from user
+echo Hello $NAME, Good Morning!
+
+Name=sagar
+readonly Name                       # readonly = overriding not allowed, showing error
+Name=DEVOPS
+echo "my name is: $Name"
+
+date                                # showing date as per ec2 region
+echo $?                             # status of last command
+echo "if o/p is 0,then this command is successful"
