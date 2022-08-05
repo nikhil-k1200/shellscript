1d8dfc1d59ea2a7d1f75c11b2e8aec46465ea2ef
nk <devopsnike@gmail.com>
2022-08-05 15:52:06 +0530

    update

--- variables.sh
+++ variables.sh
@@ -1,20 +1,24 @@
-*!/bin/bash
+#!/bin/bash
 
+#1]
 shell="bash"                        # do not leave space before & after '=' while defining variables
 echo "I am using $shell"
 echo "I am "${shell}ing
 
+#2]
 NAME="devops"
 echo My name is ${NAME}nik
 echo What is your name?
 #read NAME                          # 'read' used to get input from user
 echo Hello $NAME, Good Morning!
 
+#3]
 Name=sagar
 readonly Name                       # readonly = overriding not allowed, showing error
 Name=DEVOPS
 echo "my name is: $Name"
 
+#4]
 date                                # showing date as per ec2 region
-echo $?                             # status of last command
+echo $?                             # status of last command i.e. command run or not
 echo "if o/p is 0,then this command is successful"
